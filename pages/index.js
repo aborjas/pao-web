7a8e21a42dd5ee4f766b1bdf6e5fdab9c50f7687
Adriana Borjas <aborjasmontiel@gmail.com>
2020-09-18 17:29:43 -0400

    section div

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { Header } from "../components/header";
 import { Hero } from "../components/hero/hero";
+import { Div1 } from "components/section-div1";
 
 const PaoWeb = () => (
   <section>
     <Header />
     <Hero />
+    <Div1 />
   </section>
 );
 export default PaoWeb;
