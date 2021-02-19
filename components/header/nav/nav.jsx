0c538e855d1ccb321b3103391fd087f7d777865d
Adriana Borjas <aborjasmontiel@gmail.com>
2021-02-19 11:32:33 -0400

    burger button

--- components/header/nav/nav.jsx
+++ components/header/nav/nav.jsx
@@ -3,12 +3,63 @@ import * as React from "react";
 import styles from "./nav.module.scss";
 import Link from "next/link";
 
-export const Nav = () => {
-  return (
-    <Link href="#">
-      <a className={styles.burguerMenu}>
-        <img className={styles.icon} src="/pngs/menu.png" />
-      </a>
-    </Link>
-  );
-};
+const navList = [
+  {
+    name: "SOBRE MÍ",
+    link: "#",
+  },
+  {
+    name: "GALERÍA",
+    link: "#",
+  },
+  {
+    name: "CAPACITACIONES",
+    link: "#",
+  },
+];
+
+export class Nav extends React.Component {
+  state = {
+    isOpen: false,
+  };
+  render() {
+    return (
+      <Link href="#">
+        <div className={styles.nav}>
+          <div
+            onClick={() => this.setState({ isOpen: !this.state.isOpen })}
+            className={`${styles.btnContainer} ${
+              this.state.isOpen ? styles.isMenuOpen : ""
+            }`}
+          >
+            <div className={styles.btnUp}></div>
+            <div className={styles.btnCenter1}></div>
+            <div className={styles.btnCenter2}></div>
+            <div className={styles.btnDown}></div>
+          </div>
+        </div>
+      </Link>
+    );
+  }
+}
+
+// export const Nav = () => {
+//   const [isOpen, setIsOpen] = React.useState(false);
+//   return (
+//     <Link href="#">
+//       <div className={styles.nav}>
+//         <div
+//           onClick={() => setIsOpen(!isOpen)}
+//           className={`${styles.btnContainer} ${
+//             isOpen ? styles.isMenuOpen : ""
+//           }`}
+//         >
+//           <div className={styles.btnUp}></div>
+//           <div className={styles.btnCenter1}></div>
+//           <div className={styles.btnCenter2}></div>
+//           <div className={styles.btnDown}></div>
+//         </div>
+//       </div>
+//     </Link>
+//   );
+// };
